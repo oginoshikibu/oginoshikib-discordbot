dbe68b5a8177562dab783c71030c4ada7206ba9f
oginoshikibu <oginoshikibu@gmail.com>
2024-12-04 05:06:43 +0900

    discord botの動作を確認

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { REST, Routes } from 'discord.js';
+
 const server = Bun.serve({
     port: 3000,
     fetch(req) {
@@ -6,3 +8,48 @@ const server = Bun.serve({
 });
 
 console.log(`Listening on http://localhost:${server.port} ...`);
+
+const commands = [
+    {
+        name: 'ping',
+        description: 'Replies with Pong!',
+    },
+];
+
+if (!process.env.DISCORD_BOT_TOKEN) {
+    throw new Error('DISCORD_BOT_TOKEN is not defined');
+}
+
+if (!process.env.CLIENT_ID) {
+    throw new Error('CLIENT_ID is not defined');
+}
+
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_BOT_TOKEN);
+
+try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
+} catch (error) {
+    console.error(error);
+}
+
+import { Client, GatewayIntentBits } from 'discord.js';
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user?.tag ?? 'unknown user'}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);

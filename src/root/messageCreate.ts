569826b56a0774ca464b044365560be682ea228c
oginoshikibu <oginoshikibu@gmail.com>
2024-12-08 14:15:44 +0900

    テキトーに保存を実装

--- src/root/messageCreate.ts
+++ src/root/messageCreate.ts
@@ -1,14 +1,33 @@
+import { PrismaClient } from "@prisma/client";
 import { Message } from "discord.js"
 
 const messageCreate = async (message: Message): Promise<void> => {
     if (message.author.bot) return;
-    console.log(`Message received: ${message.content}`);
+    console.log(`Message received: (${message.createdTimestamp}) ${message.content}`);
+
+    // register blog
     if (message.channel.id === process.env.BLOG_CHANNEL_ID) {
-        try {
-            const parsedMessageURL = new URL(message.content);
-        } catch (error) {
-            message.reply('Please provide a valid URL');
-        }
+
+        const parsedMessageURL = (() => {
+            try {
+                return new URL(message.content);
+            }
+            catch (error) {
+                message.reply('Invalid URL');
+                return null;
+            }
+        })();
+        if (!parsedMessageURL) return;
+
+        const prismaClient = new PrismaClient();
+        const Blog = await prismaClient.blog.create({
+            data: {
+                url: parsedMessageURL.href,
+                title: "test",
+            },
+        });
+
+        await prismaClient.$disconnect();
     }
 }
 

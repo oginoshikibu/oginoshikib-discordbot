725e0f726f7812a1fc9df85dac323960a96135e0
oginoshikibu <oginoshikibu@gmail.com>
2024-12-16 20:52:51 +0900

    DEBUG_CHANNEL_IDへ変更

--- src/eventHandler/readyHandler.ts
+++ src/eventHandler/readyHandler.ts
@@ -2,12 +2,12 @@ import type { Client, TextChannel } from "discord.js";
 
 export const readyHandler = (client: Client): void => {
     console.log(`Logged in as ${client.user?.tag ?? 'unknown user'}`);
-    const channelId = process.env.CHANNEL_ID;
-    if (channelId) {
-        const channel = client.channels.cache.get(channelId) as TextChannel;
+    const debugChannelId = process.env.DEBUG_CHANNEL_ID;
+    if (debugChannelId) {
+        const channel = client.channels.cache.get(debugChannelId) as TextChannel;
         const currentTime = new Date().toLocaleTimeString();
         channel?.send(`Hello, world! The current time is ${currentTime}`);
     } else {
-        console.error('CHANNEL_ID is not defined');
+        console.error('DEBUG_CHANNEL_ID is not defined');
     }
 }

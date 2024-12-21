71d23887e8b6e096675392cd21cd85b01d697720
oginoshikibu <oginoshikibu@gmail.com>
2024-12-21 16:35:30 +0900

    コマンドの実行処理を追加

--- src/eventHandler/interactionCreateHandler.ts
+++ src/eventHandler/interactionCreateHandler.ts
@@ -1,9 +1,20 @@
 import type { Interaction } from "discord.js";
+import * as commandMap from '../commands';
+import type { Command } from "../commands/types";
 
 export const interactionCreateHandler = async (interaction: Interaction) => {
     if (!interaction.isCommand()) return;
 
+    // 型推論
+    const commands: Record<string, Command> = commandMap;
     const { commandName } = interaction;
 
+    try {
+        const command: Command = commands[commandName];
+        await command.execute(interaction);
+    } catch (error: any) {
+        console.error(error);
+        await interaction.reply({ content: `There was an error while executing this command! \n ${error}`, ephemeral: true });
+    }
     return;
 }

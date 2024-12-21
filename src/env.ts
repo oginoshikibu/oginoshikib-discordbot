b7a3990a5ab863d87d32b4a1aac50fcec410f77a
oginoshikibu <oginoshikibu@gmail.com>
2024-12-21 15:11:34 +0900

    環境変数のバリデーションスキーマの変数名を変更し、コードの可読性を向上

--- src/env.ts
+++ src/env.ts
@@ -1,22 +1,22 @@
 import { z } from 'zod'
 
-const zVar = z.string().min(1)
-const zEnv = z.object({
-    DISCORD_BOT_TOKEN: zVar,
-    CLIENT_ID: zVar,
-    GUILD_ID: zVar,
-    USER_ID: zVar,
-    DEBUG_CHANNEL_ID: zVar,
-    NOTIFICATION_CHANNEL_ID: zVar,
-    ARTICLE_CHANNEL_ID: zVar,
+const nonEmptyString = z.string().min(1)
+const environmentVariablesSchema = z.object({
+    DISCORD_BOT_TOKEN: nonEmptyString,
+    CLIENT_ID: nonEmptyString,
+    GUILD_ID: nonEmptyString,
+    USER_ID: nonEmptyString,
+    DEBUG_CHANNEL_ID: nonEmptyString,
+    NOTIFICATION_CHANNEL_ID: nonEmptyString,
+    ARTICLE_CHANNEL_ID: nonEmptyString,
 })
 
-const result = zEnv.safeParse(process.env)
+const result = environmentVariablesSchema.safeParse(process.env)
 if (!result.success) {
     throw `env type is invalid\n${result.error.errors.map((v) => `${v.message}: env.${v.path[0]}`).join('\n')}`
 }
 
 
 declare module 'bun' {
-    interface Env extends z.infer<typeof zEnv> { }
+    interface Env extends z.infer<typeof environmentVariablesSchema> { }
 }

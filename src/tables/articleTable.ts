08bc4c16bfdddc2e6e597f1e778ee9c6b6b5ba34
oginoshikibu <oginoshikibu@gmail.com>
2024-12-21 18:12:49 +0900

    getRandomArticle関数を追加し、ランダムな記事を取得する処理を実装

--- src/tables/articleTable.ts
+++ src/tables/articleTable.ts
@@ -21,3 +21,24 @@ export const insertArticle = async (url: string, title: string): Promise<{ artic
         console.log(`end insertArticle`);
     }
 }
+
+
+export const getRandomArticle = async (): Promise<{ article: article | null, error: any }> => {
+    console.log(`start getRandomArticle`);
+    const prismaClient = new PrismaClient();
+    try {
+        const article = await prismaClient.article.findFirst({
+            skip: Math.floor(Math.random() * await prismaClient.article.count()),
+        });
+        console.log(`Fetched article: ${article}`, { depth: 2 });
+        return { article, error: null };
+    } catch (error: any) {
+        console.error(`Failed to fetch Article: ${error.message}`);
+        return { article: null, error };
+    } finally {
+        await prismaClient.$disconnect();
+        console.log(`end getRandomArticle`);
+    }
+}
+
+    

df6d289017b0bc3f5d0f415328d3d5f540deafaa
oginoshikibu <oginoshikibu@gmail.com>
2024-12-21 16:53:54 +0900

    clientReadyHandlerを非同期関数に変更し、コマンドの登録処理を改善

--- src/eventHandler/clientReadyHandler.ts
+++ src/eventHandler/clientReadyHandler.ts
@@ -1,10 +1,11 @@
 import { REST, Routes, type Client, type TextChannel } from "discord.js";
 import { sendMessageCron } from "../services/cronServices";
-import * as Commands from '../commands';
+import * as commandMap from '../commands';
 import type { Command } from "../commands/types";
+import { createPrinter } from "typescript";
 
 
-export const clientReadyHandler = (client: Client): void => {
+export const clientReadyHandler = async (client: Client): Promise<void> => {
     console.log(`Logged in as ${client.user?.tag ?? 'unknown user'}`);
     const debugChannelId = process.env.DEBUG_CHANNEL_ID;
     if (debugChannelId) {
@@ -28,18 +29,21 @@ export const clientReadyHandler = (client: Client): void => {
     if (!CLIENT_ID) {
         throw new Error('CLIENT_ID is not defined');
     }
-    const keys = Object.keys(Commands);
+
+    // 型推論
+
     const rest = new REST({ version: '10' }).setToken(DISCORD_BOT_TOKEN);
-    keys.forEach(async (key) => {
-        const command: Command = Commands[key as keyof typeof Commands];
+    const commands: Record<string, Command> = commandMap;
+    console.log(`Started refreshing application (/) commands: ${Object.keys(commands).join(', ')}`);
+    await Promise.all(Object.keys(commands).map(async (key) => {
+        const command: Command = commands[key];
         try {
             await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: [command.data] });
             console.log(`Command ${command.data.name} registered`);
         } catch (error) {
             console.error(`Failed to register command ${command.data.name}: ${error}`);
         }
-    });
-
+    }));
 
     sendMessageCron(client, `<@${process.env.USER_ID}> どうせやる`, '0,30 9-23 * * *');
 }

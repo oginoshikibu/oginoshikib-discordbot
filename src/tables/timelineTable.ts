d5343f9944fbe3fd636050238b6cea36a3e51b24
oginoshikibu <oginoshikibu@gmail.com>
2024-12-31 20:31:59 +0900

    それっぽく1日のデータ取得の処理を追加

--- src/tables/timelineTable.ts
+++ src/tables/timelineTable.ts
@@ -5,7 +5,7 @@ import type { Timeline } from "@prisma/client";
 export const insertTimeline = async (workKindId: number, startedAt: Date, comment?: string): Promise<{ timeline: Timeline | null, error: any }> => {
     console.log(`start insertTimeline`);
     const prismaClient = new PrismaClient();
-    
+
     // Check if workKindId exists
     await prismaClient.workKind.findFirst({
         where: {
@@ -36,3 +36,39 @@ export const insertTimeline = async (workKindId: number, startedAt: Date, commen
         console.log(`end insertTimeline`);
     }
 }
+
+// 直近のworkKindIdが1（就寝）以降のデータを取得
+export const getLastDayTimeline = async (): Promise<Timeline[]> => {
+    console.log(`start getLastDayTimeline`);
+
+    const prismaClient = new PrismaClient();
+
+    try {
+        // 直近のworkKindIdが1（就寝）のデータを取得
+        const lastDayTimelines = await prismaClient.timeline.findFirstOrThrow({
+            where: {
+                workKindId: 1
+            },
+            orderBy: {
+                startedAt: 'desc'
+            }
+        }).then(async (lastSleepTimeline) => {
+            return await prismaClient.timeline.findMany({
+                where: {
+                    id: {
+                        gte: lastSleepTimeline.id
+                    }
+                }
+            });
+        });
+        console.log(`Got last day timelines: ${lastDayTimelines}`, { depth: 2 });
+        return lastDayTimelines;
+    } catch (error: any) {
+        console.error(`Failed to get last day timelines: ${error.message}`);
+        return [];
+    } finally {
+        await prismaClient.$disconnect();
+        console.log(`end getLastDayTimeline`);
+    }
+}
+
